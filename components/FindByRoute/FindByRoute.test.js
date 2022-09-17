a603862a95d4f92b09e2f3e08fcc99f083e1cbd5
Kevin Huang <krhuang2@Kevins-MacBook-Pro.local>
2022-09-17 15:34:09 -0500

    finish findbyroute tests

--- components/FindByRoute/FindByRoute.test.js
+++ components/FindByRoute/FindByRoute.test.js
@@ -1,5 +1,5 @@
 import {describe, expect, test} from '@jest/globals';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import FindByRoute from './FindByRoute';
 import '@testing-library/jest-dom/extend-expect';
 import { renderWithProviders } from '../../lib/redux/test-utils';
@@ -20,6 +20,22 @@ const routes = [
     route_label: 'route2'
   }
 ];
+
+const validDeparturesData = {
+  stops: [
+    {
+      stop_id: 56334,
+      description: 'Target Field Station Platform 2'
+    }
+  ],
+  departures: [
+    {
+      departure_text: '4:00',
+      description: 'to Mall of America',
+      route_short_name: 'Blue'
+    }
+  ]
+};
 
 global.fetch = jest.fn(() =>
   Promise.resolve({
@@ -122,6 +138,70 @@ describe('FindByRoute Component', () => {
     await user.selectOptions(screen.getByTestId('routesSelector'), route);
 
     // Assert
+    expect(await screen.findByTestId('errorMessage')).toBeInTheDocument();
+  });
+
+  test('Fetches and displays departure data if valid params are set in url', async () => {
+    // Arrange
+    const route = '901';
+    const direction = '1';
+    const placeCode = 'CODE';
+    const router = createMockRouter({
+      query: { 
+        slug: [route, direction, placeCode]
+      }
+    });
+
+    // make fetch mock to get departuresData
+    fetch.mockImplementationOnce(() => Promise.resolve({ 
+      ok: true,
+      json: () => Promise.resolve(validDeparturesData)
+    }));
+
+    renderWithProviders(
+      <RouterContext.Provider value={router}>
+        <FindByRoute routeData={routes} />
+      </RouterContext.Provider>
+    );
+    
+    // Act
+
+    // Assert
+    await waitFor(() => { 
+      expect(fetch).toHaveBeenCalledWith('https://svc.metrotransit.org/nextripv2/' + route + '/' + direction + '/' + placeCode); 
+    });
+    expect(await screen.findByTestId('departuresSection')).toBeInTheDocument();
+  });
+
+  test('Displays Error Message if departures data returns not okay', async () => {
+    // Arrange
+    const route = '901';
+    const direction = '1';
+    const placeCode = 'CODE';
+    const router = createMockRouter({
+      query: { 
+        slug: [route, direction, placeCode]
+      }
+    });
+
+    // make fetch mock to get departuresData
+    fetch.mockImplementationOnce(() => Promise.resolve({ 
+      ok: false,
+      json: () => Promise.resolve(validDeparturesData)
+    }));
+
+    renderWithProviders(
+      <RouterContext.Provider value={router}>
+        <FindByRoute routeData={routes} />
+      </RouterContext.Provider>
+    );
+    
+    // Act
+
+    // Assert
+    await waitFor(() => { 
+      expect(fetch).toHaveBeenCalledWith('https://svc.metrotransit.org/nextripv2/' + route + '/' + direction + '/' + placeCode); 
+    });
     expect(await screen.findByTestId('errorMessage')).toBeInTheDocument();
   });
 });
